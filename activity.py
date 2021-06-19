138b63e34d574fcd9436e5bb41f19938cf2d70c8
abhi0295 <abhijeet.prasad1995@gmail.com>
2021-06-19 09:58:53 +0530

    web browser open functionality added

---
+++ activity.py
@@ -0,0 +1,26 @@
+import webbrowser as wb
+import net_connect_validator
+import word_search
+
+class Activities:
+    def activityManger(self, request):
+        request = request.lower()
+        if 'open' in request:
+            return Activities().Open(request)
+
+    def Open(self,request):
+        try:
+            url, last = "https://", ".com"
+
+            key_word = word_search.KeyWord.wordMatch(request)
+            print(key_word)
+            if key_word is None:
+                return "sorry, request cannot be proceed"
+            else:
+                url = url+key_word+last
+                while net_connect_validator.checkConnectivity():
+                    wb.get().open_new(url)
+                    return "opening "+key_word
+                return "Sorry, Please check network connection"
+        except:
+            print(Exception)
--- main.py
+++ main.py
@@ -1,35 +1,55 @@
-
-
-
-import pyttsx3
 import speech_recognition as sr
-import os
 import pyttsx3 as pt
-import webbrowser
+import activity
+import  random
+
+class AmbientNoise:
+    def __init__(self):
+        try:
+            with sr.Microphone() as source:
+                sr.Recognizer().adjust_for_ambient_noise(source)
+                sr.Recognizer().energy_threshold
+        except:
+            print("error occurred in ambient try")
+
 class Recognizer:
+    requestList = ["anything else ?", "is there anything else for me ?"]
     def take_command(name):
+        AmbientNoise()
         recognize = sr.Recognizer()
-        text = "hello, ",name," what can i do for you?"
+        text = "hello,"+name+" what can i do for you?"
+        Recognizer.Speak(text)
+        firstTime = True
         with sr.Microphone() as source:
-            Recognizer.Speak(text)
-            recognize.pause_threshold = 0.7
-            audio = recognize.listen(source)
+            while True:
+                if firstTime is False:
+                    Recognizer.Speak(random.choice(Recognizer.requestList))
+                firstTime = False
+                recognize.pause_threshold = 0.9
+                audio = recognize.listen(source)
+                validity = recognize.recognize_google(audio)
+                print(validity)
+                if "no" in validity:
+                    Recognizer.Speak("Okay, shutting down myself. Have a nice day")
+                    break
 
-            try:
-                query  = recognize.recognize_google(audio)
-                new_text = "Are you saying "+query
-                Recognizer.Speak(new_text)
-            except Exception as e:
-                Recognizer.Speak("sorry say that again")
-                Recognizer.take_command("")
-
+                try:
+                    request = recognize.recognize_google(audio)
+                    print("request: "+request)
+                    response = activity.Activities().activityManger(request)
+                    print("response: "+response)
+                    Recognizer.Speak(response)
+                except Exception as e:
+                    Recognizer.Speak("sorry say that again")
+                    Recognizer.take_command("")
 
     def Speak(textToSpeech):
-        engine = pyttsx3.init()
+        engine = pt.init()
         engine.say(textToSpeech)
         engine.runAndWait()
 
     def get_name(number):
+        AmbientNoise()
         Recognizer.Speak("Hey! What's your name?")
         take_name = sr.Recognizer()
         nametext = ""
@@ -38,7 +58,6 @@ class Recognizer:
             name = take_name.listen(source)
             try:
                 nametext = take_name.recognize_google(name)
-                print("printing name "+nametext)
             except Exception as e:
                 print("exception")
                 print(e)
@@ -46,6 +65,4 @@ class Recognizer:
                 Recognizer.get_name(number)
         return nametext
 if __name__ == '__main__':
-    print("Start")
     Recognizer.take_command(Recognizer.get_name("one"))
-
